3c276869a5dac517d4a6567d4dd657fc428f8753
asd417 <ylee5@conncoll.edu>
2024-05-20 16:26:05 -0400

    ready to start

---
+++ src/craftbot/main.cpp
@@ -0,0 +1,11 @@
+#include <BWAPI.h>
+#include <BWAPI/Client.h>
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+//#include <ATen/ATen.h>
+//at::Tensor a = at::ones({ 2, 2 }, at::kInt);
+//at::Tensor b = at::randn({ 2, 2 });
+//auto c = a + b.to(at::kInt);
